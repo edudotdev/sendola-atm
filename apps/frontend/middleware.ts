3025b89c08a6af3cf3ae3eaabe7ad5d650db49de
edudotdev <eduardo.er626@gmail.com>
2024-10-25 12:07:37 -0700

    feat(refactor): remove environment variable for ease of use

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,23 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server"
+import { jwtVerify } from "jose"
 
 export async function middleware(request: NextRequest) {
-  const jwt = request.cookies.get("access_token");
+  const jwt = request.cookies.get("access_token")
 
-  if (!jwt) return NextResponse.redirect(new URL("/login", request.url));
+  if (!jwt) return NextResponse.redirect(new URL("/login", request.url))
 
   try {
-    const { payload } = await jwtVerify(
+    await jwtVerify(
       jwt.value,
-      new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET)
-    );
+      new TextEncoder().encode('secret')
+    )
     return NextResponse.next();
   } catch (error) {
     console.log(error, 'error')
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/login", request.url))
   }
 }
 
 export const config = {
   matcher: ["/", "/balance", "/withdraw", "/transactions"],
-};
+}
